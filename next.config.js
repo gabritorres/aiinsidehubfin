98add04c3f7aa5116c67dff680484c94bb41f7f9
gabritorres <gabritor@ucm.es>
2023-03-27 12:00:59 +0200

    fin

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     domains: ["res.cloudinary.com"],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path",
-        destination: `${process.env.API_URL}/:path`,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -12,9 +12,13 @@ export default function Articles({ articles }: { articles: any }) {
       }
     >
       <main className="container mx-auto my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {articles.map((article: { id: string; attributes: any }) => (
-          <Articulo key={article.id} article={article.attributes} />
-        ))}
+        {articles ? (
+          articles.data.map((article: { id: string; attributes: any }) => (
+            <Articulo key={article.id} article={article.attributes} />
+          ))
+        ) : (
+          <p>Cargando...</p>
+        )}
       </main>
     </Layout>
   );
@@ -32,7 +36,7 @@ export default function Articles({ articles }: { articles: any }) {
 
 export async function getServerSideProps() {
   try {
-    const response = await fetch(`${process.env.API_URL}/articles`);
+    const response = await fetch(`${process.env.API_URL}/articles?populate=*`);
     const articles = await response.json();
     console.log("articulos: ", articles);
     return {
@@ -41,7 +45,7 @@ export async function getServerSideProps() {
       },
     };
   } catch (error) {
-    console.error(error);
+    console.error("ERROR");
     return {
       props: {
         error: "Ocurrió un error al cargar los articulos",
