54c22e04b02ac69b451236a314adcaa8afdd9844
sirbronn26 <105813754+sirbronn26@users.noreply.github.com>
2023-04-04 12:46:46 +0200

    cookiesbn

--- components/cookieBanner.tsx
+++ components/cookieBanner.tsx
@@ -1,6 +1,7 @@
 import posthog from "posthog-js";
+import Image from "next/image";
 import { useState, useEffect } from "react";
-
+import cookie from "../public/img/cookie.svg";
 function cookieBanner() {
   const [showBanner, setShowBanner] = useState(true);
 
@@ -27,9 +28,33 @@ function cookieBanner() {
     <div>
       {showBanner && (
         <div>
-          <p>We use Cookies</p>
-          <button onClick={acceptCookies}>Accept</button>
-          <button onClick={declineCookies}>Decline</button>
+          <div className="aviso-cookies fixed bottom-4 left-4 z-50 bg-white p-4 rounded-lg w-full max-w-xs text-center shadow-xl">
+            <div className="relative bottom-12 flex justify-center items-center">
+              <Image
+                src={cookie.src}
+                width={70}
+                height={70}
+                alt="imagen logotipo"
+                className="z-10"
+              />
+            </div>
+            <h3 className="text-lg font-bold">Cookies</h3>
+            <p className="">
+              We use our own and third-party cookies to improve our services.
+            </p>
+            <button
+              className="bg-gray-800 text-white py-2 px-4 rounded font-bold transition duration-300 hover:bg-black mb-2 w-full"
+              onClick={acceptCookies}
+            >
+              Accept
+            </button>
+            <a
+              className="text-blue-500 font-bold hover:underline"
+              onClick={declineCookies}
+            >
+              Decline
+            </a>
+          </div>
         </div>
       )}
     </div>
