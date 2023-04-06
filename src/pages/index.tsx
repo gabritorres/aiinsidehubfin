b11f7f17fa84a20aefc255c3796f935f02408144
gabritorres <gabritor@ucm.es>
2023-04-06 13:12:54 +0200

    link index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -102,26 +102,33 @@ export default function Home({ articles }: { articles: any }) {
           <div className="relative flex flex-col md:flex-row w-full md:justify-beetween items-center md:justify-around mb-8 mt-8">
             <div className="ml-[20px] mr-[20px] w-full md:w-8/12">
               <div className="relative md:ml-[100px] lg:ml-[200px] md:w-3/4">
-                <div
-                  className="text-white font-bold hover:underline lg:absolute md:text-center lg:text-left lg:-ml-[10%] md:w-full lg:w-[250px] xl:w-[450px] 2xl:w-[600px] w-full z-20 uppercase sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl sm:mx-4"
-                  style={{
-                    textShadow:
-                      "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
-                  }}
-                >
-                  {atributosId1.title}
-                </div>
+                <Link href={`/articles/${atributosId1.url}`} target="_blank">
+                  <div
+                    className="text-white font-bold hover:underline lg:absolute md:text-center lg:text-left lg:-ml-[10%] md:w-full lg:w-[250px] xl:w-[450px] 2xl:w-[600px] w-full z-20 uppercase sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl sm:mx-4"
+                    style={{
+                      textShadow:
+                        "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
+                    }}
+                  >
+                    {atributosId1.title}
+                  </div>
+                </Link>
                 <div className="overflow-hidden mb-4 md:mb-0 md:mr-10 md:ml-[90px] border border-2 rounded-[40px] border-white z-8">
                   <div className="max-w-full max-h-full duration-500 hover:scale-150 ">
-                    <img
-                      src={
-                        atributosId1.image1?.data?.attributes?.formats?.medium
-                          ?.url
-                      }
-                      alt={`Imagen ${atributosId1.title}`}
-                      className="object-cover w-full h-full "
-                      style={{ objectFit: "cover" }}
-                    />
+                    <Link
+                      href={`/articles/${atributosId1.url}`}
+                      target="_blank"
+                    >
+                      <img
+                        src={
+                          atributosId1.image1?.data?.attributes?.formats?.medium
+                            ?.url
+                        }
+                        alt={`Imagen ${atributosId1.title}`}
+                        className="object-cover w-full h-full "
+                        style={{ objectFit: "cover" }}
+                      />
+                    </Link>
                     <div className="absolute inset-0 z-10 rounded-[40px] hover:box-shadow-[0 0 0 2px black] duration-500"></div>
                   </div>
                 </div>
@@ -131,38 +138,44 @@ export default function Home({ articles }: { articles: any }) {
             <div className="bg-gradient-to-r from-[#1b1d20] via-blue-500 to-[#1b1d20] h-[3px] md:h-[342px] w-full md:w-[3px] md:ml-10 md:mr-10"></div>
 
             <div className="relative flex flex-col ml-[20px] md:ml-[60px] mr-[20px] w-full md:w-4/12">
-              <div
-                className="block sm:hidden text-white font-bold hover:underline z-20 uppercase sm:text-xl md:text-2xl sm:mx-4"
-                style={{
-                  textShadow:
-                    "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
-                }}
-              >
-                {atributosId2.title}
-              </div>
+              <Link href={`/articles/${atributosId2.url}`} target="_blank">
+                <div
+                  className="block sm:hidden text-white font-bold hover:underline z-20 uppercase sm:text-xl md:text-2xl sm:mx-4"
+                  style={{
+                    textShadow:
+                      "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
+                  }}
+                >
+                  {atributosId2.title}
+                </div>
+              </Link>
               <div></div>
               <div className="overflow-hidden mb-4 md:mb-0 md:mr-10 border border-2 rounded-[40px] border-white s z-8">
                 <div className="max-w-full max-h-full duration-500 hover:scale-150">
-                  <img
-                    src={
-                      atributosId2.image1?.data?.attributes?.formats?.medium
-                        ?.url
-                    }
-                    alt={`Imagen ${atributosId2.title}`}
-                    className="object-cover w-full h-full"
-                    style={{ objectFit: "cover" }}
-                  />
+                  <Link href={`/articles/${atributosId2.url}`} target="_blank">
+                    <img
+                      src={
+                        atributosId2.image1?.data?.attributes?.formats?.medium
+                          ?.url
+                      }
+                      alt={`Imagen ${atributosId2.title}`}
+                      className="object-cover w-full h-full"
+                      style={{ objectFit: "cover" }}
+                    />
+                  </Link>
                 </div>
               </div>
-              <div
-                className="hidden sm:block text-white font-bold hover:underline z-21 uppercase sm:text-xl md:text-2xl sm:mx-4"
-                style={{
-                  textShadow:
-                    "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
-                }}
-              >
-                {atributosId2.title}
-              </div>
+              <Link href={`/articles/${atributosId2.url}`} target="_blank">
+                <div
+                  className="hidden sm:block text-white font-bold hover:underline z-21 uppercase sm:text-xl md:text-2xl sm:mx-4"
+                  style={{
+                    textShadow:
+                      "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
+                  }}
+                >
+                  {atributosId2.title}
+                </div>
+              </Link>
             </div>
           </div>
 
@@ -170,62 +183,75 @@ export default function Home({ articles }: { articles: any }) {
 
           <div className="relative flex flex-col md:flex-row w-full justify-beetween items-center md:justify-around mb-8 mt-8">
             <div className="relative flex flex-col ml-[20px] md:ml-[60px] mr-[20px] w-full md:w-4/12">
-              <div
-                className="block sm:hidden md:hidden text-white font-bold hover:underline z-20 uppercase sm:text-xl md:text-2xl sm:mx-4"
-                style={{
-                  textShadow:
-                    "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
-                }}
-              >
-                {atributosId3.title}
-              </div>
-              <div className="overflow-hidden mb-4 md:mb-0 md:mr-10 border border-2 rounded-[40px] border-white z-8">
-                <div className="max-w-full max-h-full duration-500 hover:scale-150">
-                  <img
-                    src={
-                      atributosId3.image1?.data?.attributes?.formats?.medium
-                        ?.url
-                    }
-                    alt={`Imagen ${atributosId3.title}`}
-                    className="object-cover w-full h-full"
-                    style={{ objectFit: "cover" }}
-                  />
-                </div>
-              </div>
-              <div
-                className="hidden sm:block md:block text-white font-bold hover:underline z-20 uppercase sm:text-xl md:text-2xl sm:mx-4"
-                style={{
-                  textShadow:
-                    "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
-                }}
-              >
-                {atributosId3.title}
-              </div>
-            </div>
-
-            <div className="bg-gradient-to-r from-[#1b1d20] via-blue-500 to-[#1b1d20] h-[3px] md:h-[342px] w-full md:w-[3px] md:ml-10 md:mr-10"></div>
-            <div className="ml-[20px] mr-[20px] w-full md:w-8/12">
-              <div className="relative md:ml-[100px] lg:ml-[200px] md:w-3/4">
+              <Link href={`/articles/${atributosId3.url}`} target="_blank">
                 <div
-                  className="text-white font-bold hover:underline lg:absolute md:text-center lg:text-left lg:-ml-[10%] md:w-full lg:w-[250px] xl:w-[450px] 2xl:w-[600px] w-full z-20 uppercase sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl sm:mx-4"
+                  className="block sm:hidden md:hidden text-white font-bold hover:underline z-20 uppercase sm:text-xl md:text-2xl sm:mx-4"
                   style={{
                     textShadow:
                       "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
                   }}
                 >
-                  {atributosId4.title}
+                  {atributosId3.title}
                 </div>
-                <div className="overflow-hidden mb-4 md:mb-0 md:mr-10 md:ml-[90px] border border-2 rounded-[40px] border-white z-8">
-                  <div className="max-w-full max-h-full duration-500 hover:scale-150">
+              </Link>
+              <div className="overflow-hidden mb-4 md:mb-0 md:mr-10 border border-2 rounded-[40px] border-white z-8">
+                <div className="max-w-full max-h-full duration-500 hover:scale-150">
+                  <Link href={`/articles/${atributosId3.url}`} target="_blank">
                     <img
                       src={
-                        atributosId4.image1?.data?.attributes?.formats?.medium
+                        atributosId3.image1?.data?.attributes?.formats?.medium
                           ?.url
                       }
-                      alt={`Imagen ${atributosId4.title}`}
+                      alt={`Imagen ${atributosId3.title}`}
                       className="object-cover w-full h-full"
                       style={{ objectFit: "cover" }}
                     />
+                  </Link>
+                </div>
+              </div>
+              <Link href={`/articles/${atributosId3.url}`} target="_blank">
+                <div
+                  className="hidden sm:block md:block text-white font-bold hover:underline z-20 uppercase sm:text-xl md:text-2xl sm:mx-4"
+                  style={{
+                    textShadow:
+                      "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
+                  }}
+                >
+                  {atributosId3.title}
+                </div>
+              </Link>
+            </div>
+
+            <div className="bg-gradient-to-r from-[#1b1d20] via-blue-500 to-[#1b1d20] h-[3px] md:h-[342px] w-full md:w-[3px] md:ml-10 md:mr-10"></div>
+            <div className="ml-[20px] mr-[20px] w-full md:w-8/12">
+              <div className="relative md:ml-[100px] lg:ml-[200px] md:w-3/4">
+                <Link href={`/articles/${atributosId4.url}`} target="_blank">
+                  <div
+                    className="text-white font-bold hover:underline lg:absolute md:text-center lg:text-left lg:-ml-[10%] md:w-full lg:w-[250px] xl:w-[450px] 2xl:w-[600px] w-full z-20 uppercase sm:text-xl md:text-xl lg:text-xl xl:text-2xl 2xl:text-3xl sm:mx-4"
+                    style={{
+                      textShadow:
+                        "2px 2px black, -2px -2px black, 2px -2px black, -1px 1px black",
+                    }}
+                  >
+                    {atributosId4.title}
+                  </div>
+                </Link>
+                <div className="overflow-hidden mb-4 md:mb-0 md:mr-10 md:ml-[90px] border border-2 rounded-[40px] border-white z-8">
+                  <div className="max-w-full max-h-full duration-500 hover:scale-150">
+                    <Link
+                      href={`/articles/${atributosId4.url}`}
+                      target="_blank"
+                    >
+                      <img
+                        src={
+                          atributosId4.image1?.data?.attributes?.formats?.medium
+                            ?.url
+                        }
+                        alt={`Imagen ${atributosId4.title}`}
+                        className="object-cover w-full h-full"
+                        style={{ objectFit: "cover" }}
+                      />
+                    </Link>
                   </div>
                 </div>
               </div>
