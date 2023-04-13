c1141422cab2ad5ca174ad068989432b0328fe1f
gabritorres <gabritor@ucm.es>
2023-04-13 18:46:27 +0200

    fin

--- src/pages/trust_in_scrape_insta.tsx
+++ src/pages/trust_in_scrape_insta.tsx
@@ -6,12 +6,5 @@ import fotTik from "../../public/img/fotTik.svg";
 import paypal from "../../public/img/fotPaypal.webp";
 
 export default function TrustInScrapeInsta() {
-  return (
-    <Layout
-      title={"TrustInScrapeInsta"}
-      description={"Connect with us to support you on your AI journey"}
-    >
-      <div className="">aaPPqweraasdjedadasdasASDW</div>
-    </Layout>
-  );
+  return <div className="">aaPPqweraasdjedadasdasASDW</div>;
 }
