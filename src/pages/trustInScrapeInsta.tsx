b7bd9cefdc2d73f3c21cfecfabccea82281a5338
gabritorres <gabritor@ucm.es>
2023-04-13 18:40:51 +0200

    .

---
+++ src/pages/trustInScrapeInsta.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Layout from "../../components/layout";
+import Image from "next/image";
+import fotInsta from "../../public/img/fotInsta.svg";
+import fotTik from "../../public/img/fotTik.svg";
+import paypal from "../../public/img/fotPaypal.webp";
+
+export default function TrustInScrapeInsta() {
+  return (
+    <Layout
+      title={"About Us"}
+      description={"Connect with us to support you on your AI journey"}
+    >
+      <div className="">1</div>
+    </Layout>
+  );
+}
